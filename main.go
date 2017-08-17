3fd00d0413e3acc5c033c703ccf4b38b573a061c
Yuta Shimakawa <banana.umai@gmail.com>
2017-08-17 10:08:21 +0900

    add Editor struct

--- main.go
+++ main.go
@@ -4,18 +4,67 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+type Editor struct {
+	text [][]rune
+	x int
+	y int
+}
+
+func NewEditor() *Editor {
+	return &Editor{
+		[][]rune{{}},
+		0,
+		0,
+	}
+}
+
+func (ed *Editor) AddLine() {
+	ed.text = append(ed.text, []rune{})
+	ed.x = 0
+	ed.y++
+}
+
+func (ed *Editor) MoveLeft() {
+	if ed.x == 0 {
+		return
+	}
+	ed.x--
+}
+
+func (ed *Editor) MoveRight() {
+	if len(ed.text[ed.y])-1 == ed.x {
+		return
+	}
+	ed.x++
+}
+
+func (ed *Editor) AddRune(r rune) {
+	ed.text[ed.y] = append(ed.text[ed.y], r)
+	ed.x++
+}
+
+func (ed *Editor) Draw() {
+	const color = termbox.ColorDefault
+	for i, l := range ed.text {
+		for j, r := range l {
+			termbox.SetCell(j, i, r, color, color)
+		}
+	}
+	termbox.SetCursor(ed.x, ed.y)
+	termbox.Flush()
+}
+
 func main() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
-	termbox.SetInputMode(termbox.InputEsc)
 
-	const coldef = termbox.ColorDefault
-	termbox.Clear(coldef, coldef)
+	const color = termbox.ColorDefault
+	termbox.Clear(color, color)
 
-	var x, y int
+	ed := NewEditor()
 
 	mainloop:
 	for {
@@ -25,18 +74,18 @@ func main() {
 			continue
 		}
 
-
 		switch ev.Key {
 		case termbox.KeyEsc:
 			break mainloop
 		case termbox.KeyEnter:
-			x = 0
-			y++
+			ed.AddLine()
+		case termbox.KeyArrowLeft:
+			ed.MoveLeft()
+		case termbox.KeyArrowRight:
+			ed.MoveRight()
 		default:
-			termbox.SetCell(x, y, ev.Ch, coldef, coldef)
-			termbox.Flush()
-			x++
+			ed.AddRune(ev.Ch)
 		}
-		termbox.SetCursor(x+1, y)
+		ed.Draw()
 	}
 }
